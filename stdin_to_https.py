98f18377824481f1304ba1027cbfe50296717cbe
RelaRef <choults.artiom@gmail.com>
2018-05-21 18:10:08 +0200

    stdin to https to try

---
+++ stdin_to_https.py
@@ -0,0 +1,21 @@
+import base64
+import configparser
+
+import config.globals
+from lib.latest import post
+from lib.latest import stdin_as_text
+
+Config = configparser.ConfigParser()
+Config.read('config/' + config.globals.configfile)
+
+
+def main():
+    url = Config.get('stdin', config.globals.raspberryStdInPostToUrl)
+    key = Config.get('stdin', config.globals.raspberryStdInPostId)
+    value = base64.b64encode(stdin_as_text())
+
+    result = post(url, key, value)
+    print(result)
+
+
+main()
