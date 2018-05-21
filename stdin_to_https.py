b6c6487d6a49bc2703f46dc1b2a7109adbf7c4db
RelaRef <choults.artiom@gmail.com>
2018-05-21 23:44:06 +0200

    add success condition and 1 sec delay

--- stdin_to_https.py
+++ stdin_to_https.py
@@ -4,6 +4,7 @@ import configparser
 import config.globals
 from lib.latest import post
 from lib.latest import stdin_as_byte
+import time
 
 Config = configparser.ConfigParser()
 Config.read('config/' + config.globals.configfile)
@@ -14,7 +15,13 @@ def main():
     key = Config.get('stdin', config.globals.raspberryStdInPostId)
     value = base64.b64encode(stdin_as_byte())
 
-    result = post(url, key, value)
+    try:
+        while result != 200:
+            time.sleep(1)
+            result = post(url, key, value)
+    except:
+        print('Exception.')
+
     print(result)
 
 
