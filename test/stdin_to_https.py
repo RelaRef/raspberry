a82745a599941383d3c160fdeb3d65b75603d101
RelaRef <choults.artiom@gmail.com>
2018-05-21 22:39:46 +0200

    not working :)

--- test/stdin_to_https.py
+++ test/stdin_to_https.py
@@ -1,11 +1,8 @@
 import base64
 import configparser
-import sys
-import os
 import config.globals
 from lib.latest import post
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
 
 Config = configparser.ConfigParser()
 Config.read('../config/' + config.globals.configfile)
