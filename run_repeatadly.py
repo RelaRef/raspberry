d91613456064922c1409faf9a9dbf31667f8b0c8
RelaRef <choults.artiom@gmail.com>
2018-05-25 22:49:46 +0200

    advanced run with errors dirty way cached to not interrupt the execution

--- run_repeatadly.py
+++ run_repeatadly.py
@@ -1,7 +1,33 @@
 import subprocess
+from time import sleep
 
-# bashCommand = "sudo ifconfig | python3.6 /home/pi/raspberry/stdin_to_https.py"
-bashCommand = "pwd"
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
-print(output)
+import post_to_https
+
+
+def do(bash_command):
+    try:
+        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+    except FileNotFoundError:
+        return post_to_https.do("FileNotFoundError while executing bash command: " + bash_command)
+
+    if output:
+        return post_to_https.do(output)
+    else:
+        return post_to_https.do(error)
+
+
+loop = True
+while loop:
+    try:
+        result = do("sudo ifconfig")
+    except:
+        result = "some error"
+
+    result = "result/" + str(result)
+
+    print(result)
+
+    if result.find("200"):
+        break
+    sleep(10)
